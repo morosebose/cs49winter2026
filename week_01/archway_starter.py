7d12e64712ca85fb2b08e433c5674c8b56db0551
Surajit Bose <verbose@duck.com>
2026-01-21 23:30:23 -0800

    Add week 01 slides

---
+++ week_01/archway_starter.py
@@ -0,0 +1,27 @@
+"""
+archway.py
+
+Get Karel over an archway
+
+Programmer: Surajit A. Bose, Date: April 25, 2025
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+    """
+    Move Karel from one corner of the world to the other over an archway
+    
+    Preconditions: 
+    - Karel is on the bottom row leftmost corner facing east
+    - There is an archway three squares high blocking Karel's way forward
+    
+    Postconditions:
+    - Karel is on the bottom row rightmost corner facing east 
+    - Archway is behind Karel
+    """
+    pass    # Delete this line and write your code here!  :)
+    
+    
+if __name__ == "__main__":
+    main()
