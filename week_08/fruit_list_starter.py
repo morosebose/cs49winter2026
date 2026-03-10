3a33ff0e02617c4a25aaffb7a7de4f556ad15ab4
Surajit Bose <verbose@duck.com>
2026-03-09 21:12:13 -0700

    Add week 8 slides and starter code

---
+++ week_08/fruit_list_starter.py
@@ -0,0 +1,20 @@
+def main():
+    # Create a list called `fruit_list` that contains the following fruits: 
+    # 'apple', 'banana', 'orange', 'grape', 'pineapple'.
+    
+    
+    # Print the length of the list.
+
+    
+    # Add 'mango' at the end of the list. 
+
+
+    # Check if 'honeydew' is in the list.
+    # if it is, replace it with 'melon'
+    # if it is not, add 'melon' to the second position in the list.
+
+
+    # Print the updated list one fruit at a time.
+    
+if __name__ == "__main__":
+    main()
